8b8e7e3083b09e0909da5a27caa5dc428a6ddcc9
serinryu <serin0404@naver.com>
2022-01-28 00:13:12 +0900

    회원 도메인, 리포지토리, 서비스 개발

---
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,39 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>(); //저장소
+    private static long sequence = 0L; //key 값
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence); //id를 세팅
+        store.put(member.getId(), member); //store 에 저장
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny(); //lambda 문법
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
